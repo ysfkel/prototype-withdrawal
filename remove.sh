20fa842c2c9a9ad9ee51de7a7a79414b16088e54
ysfkel <yusufkel@gmail.com>
2021-02-02 23:51:06 +0400

    update

--- remove.sh
+++
@@ -1,7 +0,0 @@
-docker system prune
-docker rmi $(docker images -a)
-docker rmi $(docker images -a -q)
-docker rm $(docker ps -a -f status=exited -q)
-docker rm $(docker ps -a -f status=created -q)
-docker rm $(docker ps -a -f -q)
-docker rm $(docker ps -a -f -q)

8178656216f988afa2dc43d57132078e70b30952
ysfkel <yusufkel@gmail.com>
2021-02-08 19:29:05 +0400

    update

--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -1,29 +1,24 @@
 import {CommonRoutesConfig} from './routes.config';
 import WithdrawalController from '../controllers/withdrawal';
 import UserController from '../controllers/user'
-import WithdrawalService from '../services/withdrawal/withdrawal';
-import UserService from '../services/user/user'
-import DenominationRepository from '../repository/denomination';
-import UserRepository from '../repository/user';
-
 import { Application } from 'express';
 
 export class WithdrawalRoutes extends CommonRoutesConfig {
-    constructor(app: Application) {
+    withdrawal: WithdrawalController
+    user: UserController
+
+    constructor(app: Application, withdrawal: WithdrawalController, user: UserController) {
         super(app, 'Withdraw');
+        this.user = user 
+        this.withdrawal = withdrawal
     }
 
     configureRoutes() {
-        const service  = new WithdrawalService(new DenominationRepository(), new UserRepository())
-        const controller = new WithdrawalController(service)
-
-        const userService  = new UserService(new UserRepository())
-        const userCcontroller = new UserController(userService)
-
+   
         this.app.route(`/withdraw/:amount`)
-            .get(controller.withdraw) 
+            .get(this.withdrawal.withdraw) 
         this.app.route(`/account/balance`)
-            .get(userCcontroller.getAccountBalance) 
+            .get(this.user.getAccountBalance) 
  
         return this.app;
     }
--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,13 @@ import { Application } from 'express';
 import * as http from 'http';
 import * as bodyparser from 'body-parser';
 import * as cors from 'cors'
-
+import WithdrawalService from './services/withdrawal/withdrawal';
+import UserService from './services/user/user'
+import DenominationRepository from './repository/denomination';
+import UserRepository from './repository/user';
 import { WithdrawalRoutes } from './api/routes';
+import WithdrawalController from './controllers/withdrawal';
+import UserController from './controllers/user'
 
 const app: Application = express();
 const server: http.Server = http.createServer(app);
@@ -13,8 +18,19 @@ const port: Number = 9000;
 app.use(bodyparser.json());
 app.use(cors());
 
-new WithdrawalRoutes(app)
+initServices(app)
  
 server.listen(port, () => {
   console.log(`server listening on port ${port}`)
 })
+
+
+function  initServices (app: Application ) {
+  const service  = new WithdrawalService(new DenominationRepository(), new UserRepository())
+  const withdrawal = new WithdrawalController(service)
+
+  const userService  = new UserService(new UserRepository())
+  const user = new UserController(userService)
+
+  new WithdrawalRoutes(app,withdrawal, user)
+}
